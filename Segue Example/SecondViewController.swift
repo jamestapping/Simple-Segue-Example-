35c554dc2e358087c72e035f5155dc7e4a2e5527
James Tapping <james.tapping@gmail.com>
2020-07-10 18:53:33 +0200

    Changed all text to 40 Helvetica and added animation to the buttons. And added a back button

--- Segue Example/SecondViewController.swift
+++ Segue Example/SecondViewController.swift
@@ -18,18 +18,24 @@ class SecondViewController: UIViewController {
         super.viewDidLoad()
 
         output.text = textToSend
+    }
+
+    @IBAction func buttonBack(_ sender: UIButton) {
+        
+        //Animate the button
+        
+        UIButton.animate(withDuration: 0.2,
+                         animations: {
+                            sender.transform = CGAffineTransform(scaleX: 0.975, y: 0.96)
+        },
+                         completion: { finish in
+                            UIButton.animate(withDuration: 0.2, animations: {
+                                sender.transform = CGAffineTransform.identity
+                            })
+        })
+        
+        self.dismiss(animated: true, completion: nil)
         
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
